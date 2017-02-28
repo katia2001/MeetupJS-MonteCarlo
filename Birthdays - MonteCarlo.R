8ab84df2c367ba05d5330ca8829bfaa415cba264
rschifini <rschifini@gmail.com>
2017-02-28 16:01:47 -0300

    Create Birthdays - MonteCarlo.R

---
+++ Birthdays - MonteCarlo.R
@@ -0,0 +1,17 @@
+ptm <- proc.time()
+
+personas = 50
+coincidencias = 2
+simulaciones = 100000
+cuenta = 0
+
+for(k in 1:simulaciones){
+    fechas = floor(runif(personas, 1, 365))
+    tst = sum(diff(sort(fechas),coincidencias-1)==0)
+    if(tst > 0) cuenta = cuenta + 1
+}
+
+resultado = cuenta/simulaciones * 100
+print(resultado)
+
+print(proc.time() - ptm)
