fd638e6dc5fd9ca5a78999c7c7c3671c878d5ce8
rschifini <rschifini@gmail.com>
2018-03-18 22:41:51 -0300

    Update Birthdays - MonteCarlo.R
    Numbers for birthdate are taken randomly from 1 to 366. Due to floor function values will range fom 1 to 365.
    Removed time measurement because it is simpler to run system.time(source(thisFileName)).

--- Birthdays - MonteCarlo.R
+++ Birthdays - MonteCarlo.R
@@ -1,17 +1,13 @@
-ptm <- proc.time()
-
 personas = 50
 coincidencias = 2
 simulaciones = 100000
 cuenta = 0
 
 for(k in 1:simulaciones){
-    fechas = floor(runif(personas, 1, 365))
+    fechas = floor(runif(personas, 1, 366))
     tst = any(diff(sort(fechas),coincidencias-1)==0)
     if(tst) cuenta = cuenta + 1
 }
 
 resultado = cuenta/simulaciones * 100
 print(resultado)
-
-print(proc.time() - ptm)
